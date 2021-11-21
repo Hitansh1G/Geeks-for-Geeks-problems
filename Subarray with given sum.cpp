267efa0f0df5b69633931255bb9aa394ea313f45
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-11-21 20:11:33 +0530

    Subarray with given sum

---
+++ Subarray with given sum.cpp
@@ -0,0 +1,32 @@
+class Solution{
+    public:
+    vector<int> subarraySum(int arr[], int n, long long s){
+        int last=0;
+        int start=0;
+        unsigned long long  currsum=0;
+        bool flag=false;
+        vector<int>res;
+        for(int i=0;i<n;i++){
+            currsum += arr[i];
+            if(currsum>=s){
+                last=i;
+                while(s<currsum && start<last){
+                    currsum -= arr[start];
+                    ++start;
+                }
+                
+                if(currsum==s){
+                    res.push_back(start + 1);
+                    res.push_back(last + 1);
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        if(flag==false)
+            res.push_back(-1);
+
+        return res;    
+    }
+};
