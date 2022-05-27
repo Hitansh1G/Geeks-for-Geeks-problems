079b44f670d81d35b779ec712e34012a950afc9e
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-05-27 21:07:16 +0530

    Intersection of two sorted Linked list

---
+++ Intersection of two sorted Linked list.cpp
@@ -0,0 +1,114 @@
+// { Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;z
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* temp1=head1;
+    Node* temp2=head2;
+    Node* temp3=new Node(-1);
+    Node* temp4=temp3;
+    while(temp1!=NULL && temp2!=NULL){
+        if(temp1->data<temp2->data){
+            temp1=temp1->next;
+        }
+        else if(temp2->data<temp1->data){
+            temp2=temp2->next;
+        }
+        else{
+            Node* n = new Node(temp1->data);
+            if(temp3->next==NULL){
+                temp3->next=n;
+                temp4=temp4->next;
+            }
+            else{
+                temp4->next=n;
+                temp4=temp4->next;
+            }
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+    }
+    return temp3->next;
+    
+}
